e0241a49cebd99542d0212ef6b0a8ec7eaa162b4
julesbruins <jf-bruins@hotmail.com>
2025-01-13 16:24:09 +0100

    begin carousel + img in druppel

---
+++ careers.js
@@ -0,0 +1,18 @@
+const prev = document.querySelector('prev-btn')
+const next = document.querySelector('next-btn')
+const list = document.querySelector('item-list')
+
+// const itemWidth = 150
+// const padding = 10
+
+// prev.addEventListener('click',()=>{
+//   list.scrollLeft -= itemWidth + padding
+// })
+
+// next.addEventListener('click',()=>{
+//   list.scrollLeft += itemWidth + padding
+// })
+
+prev.addEventListener('click') {
+    list.scrollLeft
+}
